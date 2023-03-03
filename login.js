f225147c07c3ec6d708addad81942c0e6f812a94
Anirudh Dattu <divvelaanirudh@outlook.com>
2023-03-03 13:07:50 +0530

    addition of login page

---
+++ login.js
@@ -0,0 +1,18 @@
+function validate(event) {
+  event.preventDefault();
+  const emailInput = document.querySelector('#username');
+  const passwordInput = document.querySelector('#password');
+  const emailValue = emailInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+
+  if (emailValue !== 'anirudh@123' || passwordValue !== 'anirudh') {
+    alert('Invalid email or password');
+    return false;
+  }
+
+  // Redirect to another page on successful login
+  window.location.href = 'index.html';
+}
+
+const submitButton = document.querySelector('.login__submit');
+submitButton.addEventListener('click', validate);

5cb3e66e37067f5a58ed34aacab50fc15464cc4c
Anirudh Dattu <divvelaanirudh@outlook.com>
2023-03-29 22:44:19 +0530

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ function validate(event) {
   const emailValue = emailInput.value.trim();
   const passwordValue = passwordInput.value.trim();
 
-  if (emailValue !== 'anirudh@123' || passwordValue !== 'iamgreat') {
+  if (emailValue !== 'anirudh@123' || passwordValue !== 'stupid123') {
     alert('Invalid email or password');
     return false;
   }
